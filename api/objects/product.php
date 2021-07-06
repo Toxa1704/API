ac071a586c3ce42164eac0795442ff8b9917848d
Toxa1704 <Toxa1704@outlook.com>
2021-07-06 20:21:50 +0300

    master

--- api/objects/product.php
+++ api/objects/product.php
@@ -169,5 +169,75 @@ function delete(){
 
     return false;
 }
+// метод search - поиск товаров 
+function search($keywords){
+
+    // выборка по всем записям 
+    $query = "SELECT
+                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    categories c
+                        ON p.category_id = c.id
+            WHERE
+                p.name LIKE ? OR p.description LIKE ? OR c.name LIKE ?
+            ORDER BY
+                p.created DESC";
+
+    // подготовка запроса 
+    $stmt = $this->conn->prepare($query);
+
+    // очистка 
+    $keywords=htmlspecialchars(strip_tags($keywords));
+    $keywords = "%{$keywords}%";
+
+    // привязка 
+    $stmt->bindParam(1, $keywords);
+    $stmt->bindParam(2, $keywords);
+    $stmt->bindParam(3, $keywords);
+
+    // выполняем запрос 
+    $stmt->execute();
+
+    return $stmt;
+}
+// чтение товаров с пагинацией 
+public function readPaging($from_record_num, $records_per_page){
+
+    // выборка 
+    $query = "SELECT
+                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    categories c
+                        ON p.category_id = c.id
+            ORDER BY p.created DESC
+            LIMIT ?, ?";
+
+    // подготовка запроса 
+    $stmt = $this->conn->prepare( $query );
+
+    // свяжем значения переменных 
+    $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+    $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+
+    // выполняем запрос 
+    $stmt->execute();
+
+    // вернём значения из базы данных 
+    return $stmt;
+}
+// используется для пагинации товаров 
+public function count(){
+    $query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
+
+    $stmt = $this->conn->prepare( $query );
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $row['total_rows'];
+}
 }
 ?>
